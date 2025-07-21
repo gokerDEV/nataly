e6451d156d963f76b4b9e3873fe864138f414e2b
goker <goker@cebeci.name>
2025-07-21 17:18:18 +0300

    Update version to 0.1.2 in pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="nataly",
-    version="0.1.0",
+    version="0.1.2",
     author="Göker",
     author_email="goker@goker.dev",
     description="A Python library called Nataly",
